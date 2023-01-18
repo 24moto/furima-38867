0814aef7c2edc0fd70ca3d6ea26b439b3d159cec
24moto <aveeee18@gmail.com>
2023-01-18 19:15:52 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,34 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  ZENKAKU_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
+  KATAKANA_REGEXP = /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/.freeze
+  
+  validates :password, format: {
+                         with: VALID_PASSWORD_REGEX,
+                         message: "Include both letters and numbers"
+                       }
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true,
+                        format: {
+                          with: ZENKAKU_REGEX,
+                          message: "Input full-width characters"
+                        }
+  validates :first_name, presence: true,
+                         format: {
+                           with: ZENKAKU_REGEX,
+                           message: "Input full-width characters"
+                         }
+  validates :last_name_kana, presence: true,
+                             format: {
+                               with: KATAKANA_REGEXP,
+                               message: "Input full-width katakana characters"
+                             }
+  validates :first_name_kana, presence: true,
+                              format: {
+                                with: KATAKANA_REGEXP,
+                                message: "Input full-width katakana characters"
+                              }
   validates :birth_date, presence: true
 end
