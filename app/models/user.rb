6943ea1739137e781c967a144a3383a13444c583
24moto <aveeee18@gmail.com>
2023-01-19 14:11:18 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  ZENKAKU_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
+  ZENKAKU_REGEX = /\A[ぁ-んァ-ヶ一-龥々]/.freeze
   KATAKANA_REGEXP = /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/.freeze
 
   validates :password, format: {
