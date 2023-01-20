e0e6a84c0318af287d8f6e4bb532987cb66baedc
24moto <aveeee18@gmail.com>
2023-01-20 16:52:40 +0900

    商品モデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,19 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :scheduled_delivery
 
+  validates :name,                   presence: true
+  validates :info,                   presence: true
+  validates :category_id,            numericality: { other_than: 1, message: "can't be blank" }
+  validates :sales_status_id,        numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id,          numericality: { other_than: 1, message: "can't be blank" }
+  validates :scheduled_delivery_id,  numericality: { other_than: 1, message: "can't be blank" }
+  validates :price,                  presence: true,
+                                     numericality: { 
+                                       greater_than_or_equal_to: 300,
+                                       less_than_or_equal_to: 9999999,
+                                       message: 'is out of setting range'
+                                      }
+                                     format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' }
+
 end
