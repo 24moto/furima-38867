1f609962679f8525c52a92d05f3c14e1c166a3ff
24moto <aveeee18@gmail.com>
2023-01-24 21:11:27 +0900

    金額バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,12 +19,8 @@ class Item < ApplicationRecord
   validates :shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id,          numericality: { other_than: 1, message: "can't be blank" }
   validates :scheduled_delivery_id,  numericality: { other_than: 1, message: "can't be blank" }
-  validates :price,                  presence: true,
-                                     numericality: { 
-                                       greater_than_or_equal_to: 300,
-                                       less_than_or_equal_to: 9999999,
-                                       message: 'is out of setting range'
-                                      },
-                                     format: { with: HANKAKU_NUM, message: 'is invalid. Input half-width characters' }
+  validates :price,                  presence: true
+  validates :price,                  numericality: {only_integer: true, message: 'Half-width number'}
+  validates :price,                  numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
 
 end
