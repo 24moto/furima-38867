1432633b56ee02d0f9dccab42ac782e690140cb5
24moto <aveeee18@gmail.com>
2023-01-24 21:30:54 +0900

    テストコード修正(プルダウンメニュー)

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -27,42 +27,42 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include "Info can't be blank"
       end
-      it 'category_idが空だと登録できない' do
-        @item.category_id = ''
+      it 'カテゴリーに「---」が選択されている場合は出品できない' do
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Category can't be blank"
       end
-      it 'sales_status_idが空だと登録できない' do
-        @item.sales_status_id = ''
+      it '商品の状態に「---」が選択されている場合は出品できない' do
+        @item.sales_status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Sales status can't be blank"
       end
-      it 'shipping_fee_status_idが空だと登録できない' do
-        @item.shipping_fee_status_id = ''
+      it '配送料の負担に「---」が選択されている場合は出品できない' do
+        @item.shipping_fee_status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Shipping fee status can't be blank"
       end
-      it 'prefecture_idが空だと登録できない' do
-        @item.prefecture_id = ''
+      it '発送元の地域に「---」が選択されている場合は出品できない' do
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Prefecture can't be blank"
       end
-      it 'scheduled_delivery_idが空だと登録できない' do
-        @item.scheduled_delivery_id = ''
+      it '発送までの日数に「---」が選択されている場合は出品できない' do
+        @item.scheduled_delivery_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Scheduled delivery can't be blank"
       end
-      it 'priceが空だと登録できない' do
+      it '価格が空だと登録できない' do
         @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include "Price can't be blank"
       end
-      it 'priceが300円より安いと登録できない' do
+      it '価格が300円より安いと登録できない' do
         @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include "Price is out of setting range"
       end
-      it 'priceが9999999円より高いと登録できない' do
+      it '価格が9999999円より高いと登録できない' do
         @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include "Price is out of setting range"
