b57f715bdb264ec0c61d1e2155023187f50a8df5
24moto <aveeee18@gmail.com>
2023-01-26 14:29:56 +0900

    Rubocopを実行し、check内容を自動修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :item do
     after(:build) do |item|
-      item.image.attach(io: File.open('public/images/test.jpg'),filename: 'test.jpg')
-    end 
+      item.image.attach(io: File.open('public/images/test.jpg'), filename: 'test.jpg')
+    end
     name                    { Faker::Name.initials }
     info                    { Faker::Lorem.sentence }
     category_id             { Faker::Number.between(from: 2, to: 11) }
@@ -10,7 +10,7 @@ FactoryBot.define do
     shipping_fee_status_id  { Faker::Number.between(from: 2, to: 3) }
     prefecture_id           { Faker::Number.between(from: 2, to: 48) }
     scheduled_delivery_id   { Faker::Number.between(from: 2, to: 4) }
-    price                   { Faker::Number.between(from: 300, to: 9999999) }
-    association :user 
+    price                   { Faker::Number.between(from: 300, to: 9_999_999) }
+    association :user
   end
 end
