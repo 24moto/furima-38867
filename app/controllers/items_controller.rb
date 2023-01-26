373e121dd2beecdb5b566422b7fbbd68cb4cfed5
24moto <aveeee18@gmail.com>
2023-01-26 17:51:53 +0900

    ユーザーによる遷移ページの変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -41,5 +42,11 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id,
                                  :scheduled_delivery_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless current_user.id == Item.find(params[:id]).user.id
+      redirect_to action: :index
+    end
   end
 end
