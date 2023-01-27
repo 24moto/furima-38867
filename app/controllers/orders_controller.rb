859fb5e48d6d742ac47a338e0c0154e6f7581e1c
24moto <aveeee18@gmail.com>
2023-01-27 19:44:34 +0900

    フォームの情報を保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,4 +5,20 @@ class OrdersController < ApplicationController
 		@order_address = OrderAddress.new
   end
 
+  def create
+    @order_address = OrderAddress.new(order_params)
+    binding.pry
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
